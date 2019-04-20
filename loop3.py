55fd09f5ff36aab7ee966feaab9ad62e8c013c4e
etoos <49575896+etoos@users.noreply.github.com>
2019-04-20 11:10:18 +0900

    Add files via upload

---
+++ loop3.py
@@ -0,0 +1,31 @@
+#range 함수 입력하기
+
+print("1부터 9까지")
+for i in range(1, 10, 1):
+    print(i, end=" ")
+print()
+
+print("0부터 9까지")
+for i in range (10):
+    print(i, end=" ")
+print()
+
+print("0부터 10까지")
+for i in range(11):
+    print(i, end=" ")
+print()
+
+print("1부터 10까지")
+for i in range(1,11):
+    print(i, end=" ")
+print()
+
+print("1부터 10까지")
+for i in range(1, 11, 2):
+    print(i, end=" ")
+print()
+
+print("1부터 10까지")
+for i in range(2, 11, 2):
+    print(i, end=" ")
+print()
